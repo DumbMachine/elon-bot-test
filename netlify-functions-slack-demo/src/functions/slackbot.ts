6b42a26f9fe2a756a7c7268e065c65d324ed08b9
DumbMachine <ratin.kumar.2k@gmail.com>
2022-11-28 23:15:25 +0530

    .

--- netlify-functions-slack-demo/src/functions/slackbot.ts
+++ netlify-functions-slack-demo/src/functions/slackbot.ts
@@ -99,7 +99,8 @@ const app: App = new App({
   installerOptions: {
     stateVerification: false,
   },
-  // installationStore: new FileInstallationStore(),
+  scopes: ["channels:history", "chat:write"],
+  installationStore: new FileInstallationStore(),
 });
 
 app.get("/auth", (req, res) => {});
